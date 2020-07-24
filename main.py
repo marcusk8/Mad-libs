172b198bfda5c3be9a84628c53da5a254dc04460
Marcus Kueh <marcus.k@scclc.net>
2020-07-24 18:17:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+name = input("Hello, what is your name?\n ")
+print(f"Hello {name}! Let's start." )
+
+print()
+
+holiday = input("Enter a name of a holday:\n")
+place = input("Enter a place:\n")
+hero = input("Enter a name of a hero:\n")
+outfit= input("What would you like to wear?\n")
+power = input("What super powers would you like to have?\n")
+drink = input("What drink do you like?\n")
+sport = input("what is your favorite sport\n")
+game = input ("What is your favorite game? \n")
+jacket = input ("What is your favorite jacket\n")
+
+print(f"Last {holiday} I saw {place} in my dream... but it turns out I slep walked there then I saw {hero} there and things got a bit weird. Then I saw what {hero} was wearing... {outfit}, then his supper {power} faded because he was at {place}. All of a sudden {hero} asked me if I had {drink} and I was like HECK NO why would I have {drink}, then he was asking me if I play any sports then I responed {sport} and {hero} was like {sport}!? What is the world?! So I responded {sport} is better than pretend supper hero. Then he tried to argue with me again so I told him shhhhhh im not the one that when I play {game} I fart... then {hero} responded and responed im not the one that leaves my {jacket} out and let my dog chew my {jacket} up  almost every time  I get a new one. ")
